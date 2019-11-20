6d8b7df67f2f3effa64c879661c4ec14e9ee9803
lizzab <Jjibber12>
2019-11-20 10:01:26 -0500

    Initial

---
+++ Module.Textbook.py
@@ -0,0 +1,9 @@
+# Ben Lizza
+# 11/20/19
+# Module that will include a Person class and a Textbook that will use Person class in author
+import Person from Module.Textbook
+
+class Textbook:
+    def __init__(self,title, author, edition, isbn_number, publisher, year_published, inventory, price):
+        self.title = title
+        super.
