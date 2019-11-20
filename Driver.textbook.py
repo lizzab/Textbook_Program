be856b320b598ffe5d58273e6792a344b58ba1d3
lizzab <Jjibber12>
2019-11-20 17:32:46 -0500

    First update

---
+++ Driver.textbook.py
@@ -0,0 +1,5 @@
+# Ben Lizza
+# 11/20/19
+# Driver for the Textbook module
+# create two textbooks that the user will input. Have a menu to let the user pick one of three things to do with the textbook
+
--- Module.Textbook.py
+++ Module.Textbook.py
@@ -1,9 +1,33 @@
 # Ben Lizza
 # 11/20/19
 # Module that will include a Person class and a Textbook that will use Person class in author
-import Person from Module.Textbook
 
-class Textbook:
+from Textbook_Program import Person
+
+
+class Textbook(Person):
     def __init__(self,title, author, edition, isbn_number, publisher, year_published, inventory, price):
         self.title = title
-        super.
+        super(Person).__init__(first_name=author, last_name=author, age=author)
+        self.edition = edition
+        self.isbn = isbn_number
+        self.publisher = publisher
+        self.year_published = year_published
+        self.inventory = inventory
+        self.price = price
+
+    def __init__subclass(self, add, subtract):
+        self.add = add
+        self.subtract = subtract
+        adding_inv = self.inventory + 1
+        subtracting_inv = self.inventory - 1
+        if subtract:
+            return subtracting_inv
+        elif add:
+            return adding_inv
+
+
+
+
+
+
